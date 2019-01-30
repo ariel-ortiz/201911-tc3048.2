da73808027e0236bf4e5f6b68dadeb4b22a462ef
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-01-30 17:52:49 -0600

    Example using a regular expression to strip comments from a C source file.

---
+++ 1. Lexical analysis/RegexExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class RegexExample {
+    public static void Main(String[] args) {
+        var regex = new Regex(@"([/][*].*?[*][/])|(.)",
+                              RegexOptions.Singleline);
+        var text = File.ReadAllText("hello.c");
+        foreach (Match m in regex.Matches(text)) {
+            if (m.Groups[2].Success) {
+                Console.Write(m.Value);
+            }
+        }
+    }
+}
---
+++ 1. Lexical analysis/hello.c
@@ -0,0 +1,7 @@
+/* this is my hello 
+   world program */
+	/**/
+int main(void) {
+    printf("Hello, World!"); /* end of line */
+    return 0;
+} /* end */
