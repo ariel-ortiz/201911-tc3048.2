192134ed70924b69b47cd7741f3ca30b4a2ca93c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-03-06 15:13:01 -0600

    Base files for the first practical exam.

---
+++ Exam 1/problem2.cs
@@ -0,0 +1,15 @@
+//==========================================================
+// Type your name and student ID here.
+//==========================================================
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Exam1 {
+    public class Problem2 {
+        public static void Main (String [] args) {
+
+        }
+    }
+}
