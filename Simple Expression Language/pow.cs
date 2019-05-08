087e9db092753133e7aded7eee942c8382507e52
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-05-08 17:57:13 -0500

    Code used for reverse engineering.

---
+++ Simple Expression Language/pow.cs
@@ -0,0 +1,11 @@
+using System;
+using Wyvern;
+
+class Pow {
+    static void Main() {
+        int a = 2;
+        int b = 3; 
+        int x = (int) Math.Pow(a, b);
+        Utils.Printi(x);
+    }
+}
